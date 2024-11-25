13e3b037edb4bb46ba094a62396398f7384c72a0
mvazakop <m.vazakopoulos@cosmote-evalue.gr>
2024-11-25 14:48:50 +0200

    added folders and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './pages/homepage/components/homepage/shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
